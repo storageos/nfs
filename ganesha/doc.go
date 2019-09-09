965a6f7ff364de95b7253f71343662414b68cd22
Simon Croome <simon.croome@storageos.com>
2019-09-09 10:41:27 +0100

    Update doc to remove rpcbind requirement

--- ganesha/doc.go
+++ ganesha/doc.go
@@ -4,9 +4,8 @@
 // It provides methods to Run, Close, and determine status with IsReady.
 //
 // Before starting `nfs-ganesha`, `dbus-daemon` must be running in `--system`
-// mode, and `rpcbind` must also be started.  `nfs-ganesha` seems to require
-// that `rpcbind` is running even though it is not used for NFSv4:
-// https://github.com/nfs-ganesha/nfs-ganesha/issues/114
+// mode.  You must compile `nfs-ganesha` with RPC disabled or `rpcbind` will
+// also need to be running (RPC is required for NFSv3 but not NFSv4).
 //
 // Administrative tasks are performed by interacting with `nfs-ganesha` over
 // DBus.  At the moment these actions include monitoring heartbeats to provide
